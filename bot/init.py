45fd5c07f9da07eb7ccb5d6a48a0e79dcd076dd4
igeethamadhuri2005 <igeethamadhuri2005@gmail.com>
2026-02-26 10:35:39 +0530

    Delete bot directory

--- bot/init.py
+++
@@ -1,9 +0,0 @@
-"""Trading Bot Package"""
-
-__version__ = "1.0.0"
-__author__ = "Trading Bot Developer"
-
-from bot.client import BinanceTestnetClient
-from bot.orders import OrderManager
-
-__all__ = ["BinanceTestnetClient", "OrderManager"]
